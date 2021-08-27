653130290c02d3aab72b83d9f696c8e56b7a86f1
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-08-27 10:37:10 -0400

    hello helper file

--- hello.js
+++ hello.js
@@ -1,6 +1,23 @@
-const sayHello  = function (name) {
-  console.log("Hello, " + name);
+const finalPosition = function (moves) {
+  // Your code in here ...
+  let pos = [0,0];
+  for (let i = 0; i <= moves.length; i++) {
+    if (moves[i] === 'north') {
+      pos[1] = pos[1] + 1;
+    }
+    else if (moves[i] === 'south') {
+      pos[1] = pos[1] - 1;
+    }
+    else if (moves[i] === 'east') {
+      pos[0] = pos[0] - 1;
+    }
+    else if (moves[i] === 'west') {
+      pos[0] = pos[0] + 1;
+    }
+  }
+  return pos;
 }
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+console.log(finalPosition(moves));
